5cd8305fd1baeb87764d0267478431f04e55d1aa
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2026-03-30 16:58:25 +0530

    chore(update): update deps and add vercel speed insights

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+// Vercel Speed Insights - start
+import { injectSpeedInsights } from "@vercel/speed-insights";
+injectSpeedInsights();
+// Vercel Speed Insights - end
+
 import "./style.css";
 
 // Ball animation
 // import "./canvas-ball";
 
 // particles animation
-import './particles-animation';
-
+import "./particles-animation";
